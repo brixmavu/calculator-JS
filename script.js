2b7dbd16f7d82d2fce5771dd7c8a9ad9b04126e3
brixmavu <mavubb@gmail.com>
2023-01-26 11:20:23 +0200

    added install button

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+});
+
+const installButton = document.getElementById('install-button');
+installButton.addEventListener('click', (e) => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
+});
+
+installButton.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    installButton.style.display = 'block';
+});
+
+window.addEventListener('appinstalled', (evt) => {
+    installButton.style.display = 'none';
+});
+
